0c792c497fe895501bc64b9000606eda7077f016
Zhiqiang Wang <zwang164@binghamton.edu>
2017-06-21 14:22:36 -0400

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,42 @@
+import character
+import pygame
+
+def main():
+	print(“######## Testing character model#########”)
+	player1 = character.Character()
+
+	print(“=====Standard Input Test=====”)
+	
+	if(load_image("character.png",-1))
+		print("The file is successfully loaded")
+		print(load_image("character.png",-1))
+
+	print(character.rect.x)
+	player1.move(pygame.K_a)
+	print(character.rect.x)
+		
+	print(player1.health)
+	player1.damage(50)
+	print(player1.health)
+		
+
+	player1.healthBar()
+
+	print(player1.playerHealthColor)
+	
+
+	print(“=====Out of Bounds Input Test=====”)
+	
+
+
+	print(“=====Zero Input Test=====”)
+
+
+
+	print(“=====Negative Input Test=====”)
+    
+
+	
+
+
+main()
